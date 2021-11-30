45f9576be73d4f125fad553c76f2d258a941cad9
Lester1024M <luxiongvip@163.com>
2021-11-30 13:42:22 +0800

    add App.clss

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+/**
+ * @author Lester
+ * @version 1.0
+ * @date 2021-11-30
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("good123");
+    }
+}
